0893c532ef8da46b38a3fba609cf452e5540c3a6
Anna Anks Nowak <matisnape@wp.pl>
2019-04-16 13:33:24 +0200

    Remove unnecessary methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'fundraisers#index'
 
-  resources :fundraisers
+  resources :fundraisers, only: [:index, :show, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
