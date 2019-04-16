8040bddeb27d1ac1056faab3d2334ebac8c5619e
Anna Anks Nowak <matisnape@wp.pl>
2019-04-16 15:40:45 +0200

    Add controller tests for fundraisers

--- Gemfile
+++ Gemfile
@@ -40,12 +40,14 @@ gem 'bootstrap', '~> 4.3.1'
 gem 'jquery-rails'
 gem 'httparty'
 
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   # gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'pry'
   gem 'rspec-rails'
   gem 'database_cleaner'
+  gem 'rails-controller-testing'
 end
 
 group :development do
--- spec/controllers/fundraisers_controller_spec.rb
+++ spec/controllers/fundraisers_controller_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe FundraisersController, type: :controller do
+  fixtures :fundraisers, :payments
+  render_views
 
+  it "GET #index renders index template" do
+    get :index
+    expect(response).to render_template :index
+    expect(response.status).to eq 200
+  end
+
+  it "GET #show renders show template" do
+    get :show, params: { id: Fundraiser.first.id }
+    expect(response).to render_template :show
+    expect(response.status).to eq 200
+  end
 end
