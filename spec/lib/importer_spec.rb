5a9e0440b8f0a574cbcd23e54fb31c959ade7a6d
Anna Anks Nowak <matisnape@wp.pl>
2019-04-22 19:43:03 +0200

    Bring back parentheses to importer spec due to chaining

--- spec/lib/importer_spec.rb
+++ spec/lib/importer_spec.rb
@@ -42,7 +42,7 @@ RSpec.describe ImportFromAPI do
 
   it "imports one fundraiser to db with payments" do
     expect { new_import.run! }
-      .to change { Fundraiser.count }.by 1
+      .to change { Fundraiser.count }.by(1)
       .and change { Payment.count }.by payments.count
 
     expect(Fundraiser.first).to have_attributes(
