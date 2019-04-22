08067f0016a586311834b89623379f5fb00a2792
Anna Anks Nowak <matisnape@wp.pl>
2019-04-22 19:11:25 +0200

    Update fundraiser spec to support 2 payment pages

--- spec/lib/importer_spec.rb
+++ spec/lib/importer_spec.rb
@@ -3,9 +3,11 @@ require './lib/importer'
 
 RSpec.describe ImportFromAPI do
   let(:new_import) { ImportFromAPI.new }
-  let(:response) { File.read("#{::Rails.root}/spec/fixtures/gramy17.json") }
-  let(:parsed_response) { JSON.parse(response) }
-  let(:payments) { parsed_response["payments"] }
+  let(:response_first_page) { File.read("#{::Rails.root}/spec/fixtures/gramy17.json") }
+  let(:response_second_page) { File.read("#{::Rails.root}/spec/fixtures/gramy17_second_page.json") }
+  let(:parsed_response_first_page) { JSON.parse(response_first_page) }
+  let(:parsed_response_second_page) { JSON.parse(response_second_page) }
+  let(:payments) { parsed_response_first_page["payments"] + parsed_response_second_page["payments"] }
 
   before do
     stub_request(:get, "https://www.siepomaga.pl/gramy17.json?page=1").
@@ -17,7 +19,19 @@ RSpec.describe ImportFromAPI do
         }).
       to_return(
         status: 200,
-        body: response,
+        body: response_first_page,
+        headers: { 'Content-Type' => 'application/json' }
+      )
+    stub_request(:get, "https://www.siepomaga.pl/gramy17.json?page=2").
+      with(
+        headers: {
+          'Accept'=>'*/*',
+          'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+          'User-Agent'=>'Ruby'
+        }).
+      to_return(
+        status: 200,
+        body: response_second_page,
         headers: { 'Content-Type' => 'application/json' }
       )
   end
@@ -32,7 +46,7 @@ RSpec.describe ImportFromAPI do
       .and change { Payment.count }.by(payments.count)
 
     expect(Fundraiser.first).to have_attributes(
-      parsed_response.slice(
+      parsed_response_first_page.slice(
         "title", "description",
         "funds_aim", "funds_current", "funds_percentage",
         "payments_total_count"
